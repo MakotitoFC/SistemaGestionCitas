faa21e5fa41f6347561f4744f241e4734d147cff
MakotitoFC <makotitofc@gmail.com>
2025-12-18 15:59:49 -0500

    Add files via upload

---
+++ lib/client.ts
@@ -0,0 +1,13 @@
+import { createBrowserClient } from "@supabase/ssr"
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
+
+export const createClient = () => createBrowserClient(
+  supabaseUrl,
+  supabaseAnonKey
+)
